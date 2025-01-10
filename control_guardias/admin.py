b2a021374d7bbefab6140f5e88ba12f482ee5c8e
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-01-10 11:58:53 -0300

    Eliminar el campo 'dia_semana' del modelo 'Guardia' y ajustar el campo 'fecha' para que no tenga valor por defecto. Actualizar las plantillas y el administrador para reflejar estos cambios.

--- control_guardias/admin.py
+++ control_guardias/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Guardia
 
 class GuardiaAdmin(admin.ModelAdmin):
-    list_display = ('fecha', 'dia_semana', 'franja_horaria', 'cubierta', 'medico')
-    list_filter = ('fecha', 'dia_semana', 'franja_horaria', 'cubierta')
+    list_display = ('fecha', 'franja_horaria', 'cubierta', 'medico')
+    list_filter = ('fecha', 'franja_horaria', 'cubierta')
     search_fields = ('medico',)
 
 admin.site.register(Guardia, GuardiaAdmin)
--- control_guardias/models.py
+++ control_guardias/models.py
@@ -1,27 +1,15 @@
 from django.db import models
 
-# Create your models here.
 class Guardia(models.Model):
-    DIA_SEMANA_CHOICES = [
-        ('LUN', 'Lunes'),
-        ('MAR', 'Martes'),
-        ('MIE', 'Miércoles'),
-        ('JUE', 'Jueves'),
-        ('VIE', 'Viernes'),
-        ('SAB', 'Sábado'),
-        ('DOM', 'Domingo'),
-    ]
-
     FRANJA_HORARIA_CHOICES = [
         ('NOCHE', '20:00 - 08:00'),
         ('DIA_COMPLETO', '24 horas'),
     ]
 
-    dia_semana = models.CharField(max_length=3, choices=DIA_SEMANA_CHOICES)
     franja_horaria = models.CharField(max_length=12, choices=FRANJA_HORARIA_CHOICES)
     cubierta = models.BooleanField(default=False)
     medico = models.CharField(max_length=100, blank=True, null=True)
-    fecha = models.DateField(default=None)
+    fecha = models.DateField()
 
     def __str__(self):
-        return f"{self.fecha.strftime('%A %d/%m/%y')} - {self.get_dia_semana_display()} - {self.get_franja_horaria_display()} - {'Cubierta' if self.cubierta else 'No cubierta'}"
+        return f"{self.fecha.strftime('%A %d/%m/%y')} - {self.get_franja_horaria_display()} - {'Cubierta' if self.cubierta else 'No cubierta'}"
