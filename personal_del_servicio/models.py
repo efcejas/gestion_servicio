23b3bc043c36f9a119d8df74b5b98cf3aea59310
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-01-10 18:22:51 -0300

    Hacer campos del modelo 'personal_medico' opcionales al permitir valores nulos y vacíos

--- personal_del_servicio/models.py
+++ personal_del_servicio/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class personal_medico (models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    dni = models.IntegerField(unique=True)
-    telefono = models.IntegerField()
-    email = models.EmailField()
-    matricula = models.IntegerField(unique=True)
-    observaciones = models.TextField()
+    dni = models.IntegerField(unique=True, blank=True, null=True)
+    telefono = models.IntegerField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    matricula = models.IntegerField(unique=True, blank=True, null=True)
+    observaciones = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Personal Médico'
