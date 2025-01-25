7fd7e5dd2f53fe78515c8e1f1660361c6ab681ea
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-01-25 16:36:21 -0300

    Agregar botón para finalizar y mostrar mensajes en el formulario de registro de estudios

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, TemplateView
 from django.contrib.messages.views import SuccessMessageMixin
