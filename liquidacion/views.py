45f0105417b21bc00c4748f8a3b327a87e4732e3
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-09-29 12:48:33 -0300

    refactor: desactivar importaciones de pandas y CargaExcelForm debido a la carga masiva pausada

--- liquidacion/views.py
+++ liquidacion/views.py
@@ -9,7 +9,7 @@ from django.db.models import Sum, Count, Q, Prefetch
 from django.http import FileResponse, HttpResponse
 from django.contrib.auth import get_user_model
 import io, json
-import pandas as pd
+# import pandas as pd  # Desactivado (carga masiva pausada)
 from datetime import datetime, date
 from collections import defaultdict
 from reportlab.lib.pagesizes import letter
@@ -26,7 +26,7 @@ from .forms import (
     FiltroProcedimientosIntervensionismoForm, 
     FiltroEstudiosPorMedicoForm,
     DiaSinPacientesForm,
-    CargaExcelForm,
+    # CargaExcelForm,  # Desactivado (carga masiva pausada)
 )
 import openpyxl
 from openpyxl.styles import Alignment, Font
